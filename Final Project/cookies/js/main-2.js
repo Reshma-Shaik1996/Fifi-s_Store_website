f81739b5a6a2679baf48723e3e78a395a823855d
Shaik Reshma <reshmashaik96@gmail.com>
2020-02-03 10:22:33 -0500

    sync

--- Final Project/cookies/js/main-2.js
+++
@@ -1,8 +0,0 @@
-// var name = localStorage.getItem("contactName");
-
-// document.getElementById("userName").textContent = name;
-var obj = localStorage.getItem("contactInfo");
-
-document.getElementById("usrName").textContent = obj.userName;
-document.getElementById("uEmail").textContent = obj.email;
-document.getElementById("tel").textContent = obj.tel;
